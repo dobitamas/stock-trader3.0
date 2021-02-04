c5b7d8c88581c00f93bc0924fddfdaccc6afc458
TiborGulyas <gulyastibor@gmail.com>
2021-02-04 13:43:00 +0100

    fixing sidebar and dashboard

--- stocktrader/src/App.js
+++ stocktrader/src/App.js
@@ -51,30 +51,12 @@ function App() {
               </li>
               <li className="nav-item">
                 <a className="nav-link" >
-                  <span data-feather="shopping-cart"></span>
-                  Products
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" >
-                  <span data-feather="users"></span>
-                  Customers
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" >
                   <span data-feather="bar-chart-2"></span>
                   Reports
                 </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" >
-                  <span data-feather="layers"></span>
-                  Integrations
-                </a>
-              </li>
             </ul>
-
+          {/*
             <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
               <span>Saved reports</span>
               <a className="d-flex align-items-center text-muted" >
@@ -107,12 +89,14 @@ function App() {
                 </a>
               </li>
             </ul>
+          */}
           </div>
         </nav>
 
         <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 className="h2">Dashboard</h1>
+            {/*
             <div className="btn-toolbar mb-2 mb-md-0">
               <div className="btn-group mr-2">
                 <button className="btn btn-sm btn-outline-secondary">Share</button>
@@ -123,6 +107,7 @@ function App() {
                 This week
               </button>
             </div>
+            */}
           </div>
           <Router>
             <div className="my-4 w-100" width="900" height="380">
